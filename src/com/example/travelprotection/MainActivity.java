9c09fd04c8c00931b00c66c3894dd34446fde8d7
liuyuwei <liuyuwei@xueyuan-xy-D047.corp.qihoo.net>
2015-08-26 11:35:04 +0800

    id button

--- src/com/example/travelprotection/MainActivity.java
+++ src/com/example/travelprotection/MainActivity.java
@@ -2,8 +2,7 @@ package com.example.travelprotection;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,25 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button b=(Button)findViewById(R.id.button1);
+        b.setBackgroundColor(0x77FF0000);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
